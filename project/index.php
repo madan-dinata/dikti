517f44901f96b9bb21a9555c7576932a5066e041
fredysiswanto <idloginfredy@gmail.com>
2019-07-11 21:08:56 +0700

    woke

--- project/index.php
+++ project/index.php
@@ -1,9 +1,23 @@
 <?php
+// koneksi ke database
+// Create database connection using config file
+include_once("database/koneksi.php");
+$data = query("SELECT * FROM barang");
+// var_dump($data);
+
+// Fetch all users data from database
+// $barang = query($db, "SELECT * FROM barang");
+// $data = mysqli_fetch_object($result);
+
+
+// mengambil template layout
 require_once 'layout/header.php';
 require_once 'layout/nav_top.php';
 require_once 'layout/nav_right.php';
 // slide hanya untuk home
 require_once 'layout/slide.php';
+// var_dump($barang);
+// die();
 
 ?>
 
@@ -12,24 +26,24 @@ require_once 'layout/slide.php';
 
 
 <div class="row">
-
-  <div class="col-lg-4 col-md-6 mb-4">
-    <div class="card h-100">
-      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-      <div class="card-body">
-        <h4 class="card-title">
-          <a href="#">Item One</a>
-        </h4>
-        <h5>$24.99</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-          numquam aspernatur!</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+  <?php foreach ($data as $item) : ?>
+    <div class="col-lg-4 col-md-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="assets/img/<?= $item['img']; ?>" alt=""></a>
+        <div class="card-body">
+          <h4 class="card-title">
+            <a href="#"><?= $item['nama_barang']; ?></a>
+          </h4>
+          <h5>Rp.<?= $item['harga']; ?></h5>
+          <p class="card-text"><?= $item['spesifikasi']; ?></p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+        </div>
       </div>
     </div>
-  </div>
-
+  <?php endforeach; ?>
+  <!-- 
   <div class="col-lg-4 col-md-6 mb-4">
     <div class="card h-100">
       <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
@@ -113,7 +127,7 @@ require_once 'layout/slide.php';
         <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
       </div>
     </div>
-  </div>
+  </div> -->
 
 </div>
 
